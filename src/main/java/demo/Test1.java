2bf72c1cf078b3b383bb383029b0caf0f3d14131
meghana BA <meghana BA@DESKTOP-11PLTCP>
2023-03-21 16:33:10 +0530

    commit test2 to branch1

--- src/main/java/demo/Test1.java
+++ src/main/java/demo/Test1.java
@@ -2,7 +2,7 @@ package demo;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		int a=10;
 		int b=23;
 		int sum=a+b;
---
+++ src/main/java/demo/Test2.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=37;
+		int result= a>b?a:b;
+		
+		System.out.println("greater number is" +result);
+
+	}
+
+}
